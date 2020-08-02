b7397cbf64aef08c1c895695f1ae983ef627f27e
Ikealandian <66649050+Ikealandian@users.noreply.github.com>
2020-08-03 01:25:32 +0200

    IVulkan GetKHRSurface

--- src/Vulkan.hpp
+++ src/Vulkan.hpp
@@ -1,6 +1,7 @@
 #ifndef _KOT_VULKAN_INTERFACE_
 #define _KOT_VULKAN_INTERFACE_
 
+#include <vulkan/vulkan.hpp>
 #include "Window.hpp"
 
 typedef class __IVkInterface
@@ -15,6 +16,14 @@ private:
     // Platform Implementation
     struct __IVkImpl* _vkImpl;
     unsigned _vkResult;
+
+public:
+    virtual ~__IVkInterface() = default;
+
+    virtual vk::SurfaceKHR* GetKHRSurface() = 0;
+
+public:
+    static __IVkInterface* Create(const __IVkInterface::Attributes& _Attributes);
 
 }IVulkan;
 
