e312e9cd0b439e4a5d7be477036a0a09ee47177c
Ikealandian <66649050+Ikealandian@users.noreply.github.com>
2020-08-05 22:07:39 +0200

    Update Build File

--- build_vulkan_linux.mk
+++ build_vulkan_linux.mk
@@ -1,5 +1,5 @@
 # mkbld -f build.mk -c g++
-# 	g++ -Wall -Wextra -fsanitize=leak -I src/ -I platform/ -L/usr/X11R6/lib -lX11 -L/usr/vulkan/lib -lvulkan src/Window.cpp src/Vulkan.cpp platform/X11/X11Window.cpp platform/X11/X11Vulkan.cpp Examples/VulkanTest.cpp -o out/Linux.o
+# 	g++ -Wall -Wextra -fsanitize=leak -I src/ -I platform/ -L/usr/X11R6/lib -lX11 -L/usr/vulkan/lib -lvulkan src/Window.cpp src/Vulkan.cpp platform/X11/X11Window.cpp platform/X11/X11Vulkan.cpp examples/VulkanTest.cpp -o out/Linux.o
 # mkbld -run
 #	./out/Linux.o
 
@@ -26,7 +26,7 @@ include_directories =
 
 source_files = 
 {
-	"Examples/VulkanTest.cpp",
+	"examples/VulkanTest.cpp",
 	"src/Window.cpp",
 	"src/Vulkan.cpp",
 	"platform/X11/X11Window.cpp",
