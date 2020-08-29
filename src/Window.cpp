315609744c5f379471a88e5a20a5bd87c6b25ea8
Ikealandian <66649050+Ikealandian@users.noreply.github.com>
2020-08-29 02:49:19 +0200

    Support creating IWinWindow

--- src/Window.cpp
+++ src/Window.cpp
@@ -1,7 +1,17 @@
+#include "Platform.hpp"
 #include "Window.hpp"
-#include "X11/X11Window.hpp"
+
+#ifdef _KOT_PLATFORM_LINUX
+#   include "X11/X11Window.hpp"
+using PlatformWindow = IX11Window;
+#endif
+
+#ifdef _KOT_PLATFORM_WINDOWS
+#   include "Windows/WinWindow.hpp"
+using PlatformWindow = IWinWindow;
+#endif
 
 __IWinInterface* __IWinInterface::Create(const __IWinInterface::Attributes& _Attributes)
 {
-    return new IX11Window(_Attributes);
+    return new PlatformWindow(_Attributes);
 }
