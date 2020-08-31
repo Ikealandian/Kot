e5c576da6ae36900ac40840326513eea56b51d0c
Ikealandian <66649050+Ikealandian@users.noreply.github.com>
2020-08-30 23:23:55 -0700

    Create EventStack.hpp

---
+++ platform/EventStack.hpp
@@ -0,0 +1,53 @@
+#ifndef _KOT_EVENT_STACK_
+#define _KOT_EVENT_STACK_
+
+#include <Events.hpp>
+#include <stack>
+
+typedef class __UEventStack
+{
+private:
+	std::stack<WEvent> Stack;
+	WEvent Last;
+
+public:
+
+	__UEventStack();
+	~__UEventStack();
+
+	void PushEvent(const WEvent& _Event);
+	WEvent PopEvent();
+
+	void PushWindowCreatedEvent();
+	void PushWindowCloseEvent();
+	void PushWindowExposedEvent();
+	void PushWindowMinimizedEvent();
+	void PushWindowMaximizedEvent();
+
+	void PushPointerInEvent();
+	void PushPointerOutEvent();
+
+	void PushCharacterInputEvent(char& _Character);
+
+	void PushKeyInputEvent(Keys& _Key, KeyAction& _Action);
+
+	void PushButtonInputEvent(
+		Buttons& _Button, ButtonAction& _Action,
+		int& _PointerX, int& PointerY
+	);
+
+	void PushMouseScrollEvent(
+		float& _Delta, Scroll& _Direction,
+		int& _PointerX, int& PointerY
+	);
+
+	void PushPointerMovedEvent(int& _PointerX, int& PointerY);
+
+	void PushWindowChangedEvent(
+		int& _X, int& _Y,
+		int& _Width, int& _Height
+	);
+
+}EventStack;
+
+#endif
