2397154d75977ab03711d0d55a3327f984b1221f
Ikealandian <66649050+Ikealandian@users.noreply.github.com>
2020-08-31 00:58:52 -0700

    Replace EventStack:: with __UEventStack:: to fix VS intellisense

--- platform/EventStack.cpp
+++ platform/EventStack.cpp
@@ -1,69 +1,69 @@
 #include "EventStack.hpp"
 
-EventStack::__UEventStack()
+__UEventStack::__UEventStack()
 	: eLast({ WEventType::NoEvent }),
 	  pX(0), pY(0)
 {
 }
 
-EventStack::~__UEventStack()
+__UEventStack::~__UEventStack()
 {
 }
 
-bool EventStack::IsEventStackNotEmpty() const
+bool __UEventStack::IsEventStackNotEmpty() const
 {
 	return !(Stack.empty());
 }
 
-void EventStack::PushEvent(const WEvent& _Event)
+void __UEventStack::PushEvent(const WEvent& _Event)
 {
 	eLast = _Event;
 	Stack.push(_Event);
 }
 
-WEvent EventStack::PopEvent()
+WEvent __UEventStack::PopEvent()
 {
 	WEvent& eTop = Stack.top();
 	Stack.pop();
 	return eTop;
 }
 
-void EventStack::PushWindowCreatedEvent()
+void __UEventStack::PushWindowCreatedEvent()
 {
 	PushEvent({ WEventType::WindowCreated });
 }
 
-void EventStack::PushWindowCloseEvent()
+void __UEventStack::PushWindowCloseEvent()
 {
 	PushEvent({ WEventType::WindowClosed });
 }
 
-void EventStack::PushWindowExposedEvent()
+void __UEventStack::PushWindowExposedEvent()
 {
 	PushEvent({ WEventType::WindowExposed });
 }
 
-void EventStack::PushWindowMinimizedEvent()
+void __UEventStack::PushWindowMinimizedEvent()
 {
 	PushEvent({ WEventType::WindowMinimized });
 }
 
-void EventStack::PushWindowMaximizedEvent()
+void __UEventStack::PushWindowMaximizedEvent()
 {
 	PushEvent({ WEventType::WindowMaximized });
 }
 
-void EventStack::PushPointerInEvent()
+void __UEventStack::PushPointerInEvent()
 {
 	PushEvent({ WEventType::PointerIn });
 }
 
-void EventStack::PushPointerOutEvent()
+void __UEventStack::PushPointerOutEvent()
 {
 	PushEvent({ WEventType::PointerOut });
 }
 
-void EventStack::PushCharacterInputEvent(char& _Character)
+void __UEventStack::PushCharacterInputEvent(char& _Character)
 {
 	WEvent CharEvent = { WEventType::CharEvent };
 	CharEvent.eChar = _Character;
@@ -71,7 +71,7 @@ void EventStack::PushCharacterInputEvent(char& _Character)
 	PushEvent(CharEvent);
 }
 
-void EventStack::PushKeyInputEvent(Keys& _Key, KeyAction& _Action)
+void __UEventStack::PushKeyInputEvent(Keys& _Key, KeyAction& _Action)
 {
 	WEvent KeyEvent = { WEventType::KeyEvent };
 	KeyEvent.eKey.Code = _Key;
@@ -80,7 +80,7 @@ void EventStack::PushKeyInputEvent(Keys& _Key, KeyAction& _Action)
 	PushEvent(KeyEvent);
 }
 
-void EventStack::PushButtonInputEvent(Buttons& _Button, ButtonAction& _Action, int& _PointerX, int& _PointerY)
+void __UEventStack::PushButtonInputEvent(Buttons& _Button, ButtonAction& _Action, int& _PointerX, int& _PointerY)
 {
 	WEvent ButtonEvent = { WEventType::KeyEvent };
 	ButtonEvent.eButton.Code = _Button;
@@ -91,7 +91,7 @@ void EventStack::PushButtonInputEvent(Buttons& _Button, ButtonAction& _Action, i
 	PushEvent(ButtonEvent);
 }
 
-void EventStack::PushMouseScrollEvent(float& _Delta, Scroll& _Direction, int& _PointerX, int& _PointerY)
+void __UEventStack::PushMouseScrollEvent(float& _Delta, Scroll& _Direction, int& _PointerX, int& _PointerY)
 {
 	WEvent ScrollEvent = { WEventType::ScrollEvent };
 
@@ -121,7 +121,7 @@ void EventStack::PushMouseScrollEvent(float& _Delta, Scroll& _Direction, int& _P
 	PushEvent(ScrollEvent);
 }
 
-void EventStack::PushPointerMovedEvent(int& _PointerX, int& _PointerY)
+void __UEventStack::PushPointerMovedEvent(int& _PointerX, int& _PointerY)
 {
 	WEvent PointerMovedEvent = { WEventType::PointerMoved };
 
@@ -134,7 +134,7 @@ void EventStack::PushPointerMovedEvent(int& _PointerX, int& _PointerY)
 	PushEvent(PointerMovedEvent);
 }
 
-void EventStack::PushWindowChangedEvent(int& _X, int& _Y, int& _Width, int& _Height)
+void __UEventStack::PushWindowChangedEvent(int& _X, int& _Y, int& _Width, int& _Height)
 {
 	WEvent WindowChangedEvent = { WEventType::WindowChanged };
 
