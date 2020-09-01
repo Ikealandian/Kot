958cbc80e89011f69fbc3881dacbb99536780423
Ikealandian <66649050+Ikealandian@users.noreply.github.com>
2020-09-01 00:38:00 -0700

    Rename CursorMode and CursorState declaration variables

--- platform/Windows/WinWindow.hpp
+++ platform/Windows/WinWindow.hpp
@@ -32,8 +32,8 @@ public:
     virtual unsigned GetPlatform() const override;
     virtual DisplayScreen* GetDisplayData() const override;
 
-    virtual void SetCursorMode(const CursorMode& _Cursor) override;
-    virtual void SetCursorState(const CursorState& _Cursor) override;
+    virtual void SetCursorMode(const WCursorMode& _Cursor) override;
+    virtual void SetCursorState(const WCursorState& _Cursor) override;
 
     virtual void* SetNativeData(void* _Data) override;
     virtual void* GetNativeData() const override;
