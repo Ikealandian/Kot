eca1669e8e9833825d60f8abf31493d13722dab8
Ikealandian <66649050+Ikealandian@users.noreply.github.com>
2020-08-31 23:13:11 -0700

    Implement EventHandler

---
+++ src/EventHandler.cpp
@@ -0,0 +1,39 @@
+#include <EventHandler.hpp>
+#include <ImplOutline.hpp>
+#include "EventHandler.hpp"
+
+#include <functional>
+
+struct __IWinImpl : public ImplOutline {};
+
+void __UEventHandler::OnWindowUpdateEnd(void)
+{
+	EventStack StackCopy = Window->_wImpl->eStack;
+	long Position = 0;
+
+	while (StackCopy.IsEventStackNotEmpty())
+	{
+		WEvent Event = StackCopy.PopEvent();
+		if (Callbacks.find(Event.Type) != Callbacks.end())
+		{
+			if (Callbacks[Event.Type](Window->_wImpl->eStack.PokeEvent(Position)))
+				return;
+		}
+		Position++;
+	}
+}
+
+__UEventHandler::__UEventHandler(IWindow** _Window)
+{
+	Window = *_Window;
+	Window->_wImpl->uCallback = std::bind(&__UEventHandler::OnWindowUpdateEnd, this);
+}
+
+__UEventHandler::~__UEventHandler()
+{
+}
+
+void __UEventHandler::AddCallback(const WEventType& _Event, const EventCallback& _Callback)
+{
+	Callbacks[_Event] = _Callback;
+}
